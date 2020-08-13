54641c63590988ce178d0fd8574c05409e4bef46
yusuke mitsugi <pastabal.jungle@gmail.com>
2020-08-13 15:27:06 +0900

    Initial Commit

---
+++ PageControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PageControl
+//
+//  Created by Yusuke Mitsugi on 2020/08/13.
+//  Copyright © 2020 Yusuke Mitsugi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
